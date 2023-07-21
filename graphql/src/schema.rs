0291ea614d06d2b414a9c292afe7f72d8df730f3
Daniel Granhão <daniel.granhao@gmail.com>
2023-07-21 16:57:34 +0100

    Add email registration mutation

--- graphql/src/schema.rs
+++ graphql/src/schema.rs
@@ -79,6 +79,14 @@ pub struct RegisterNode;
     query_path = "schemas/operations.graphql",
     response_derives = "Debug"
 )]
+pub struct RegisterEmail;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schemas/schema_wallet_read.graphql",
+    query_path = "schemas/operations.graphql",
+    response_derives = "Debug"
+)]
 pub struct GetBusinessOwner;
 
 #[derive(GraphQLQuery)]
