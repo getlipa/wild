f3c68ee9457a303baf437a2420a04aa32d161664
Andrei <andrei.i@posteo.de>
2023-09-15 00:00:00 +0000

    Add funds migration operations

--- graphql/src/schema.rs
+++ graphql/src/schema.rs
@@ -173,3 +173,19 @@ type float8 = f64;
     response_derives = "Debug"
 )]
 pub struct ListUncompletedTopups;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schemas/schema_wallet_read.graphql",
+    query_path = "schemas/operations.graphql",
+    response_derives = "Debug"
+)]
+pub struct MigrationBalance;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "schemas/schema_wallet_read.graphql",
+    query_path = "schemas/operations.graphql",
+    response_derives = "Debug"
+)]
+pub struct MigrateFunds;
