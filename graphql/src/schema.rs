dd039f149be09eed781f48f00d73506ed3edd964
Dominic Leutenegger <dominic@getlipa.com>
2023-11-09 16:15:23 +0100

    Adjust chameleon to schema changes

--- graphql/src/schema.rs
+++ graphql/src/schema.rs
@@ -165,6 +165,8 @@ pub struct GetLatestChannelManager;
 #[allow(non_camel_case_types)]
 type bigint = u64;
 type BigInteger = bigint;
+#[allow(non_camel_case_types)]
+type float8 = f64;
 
 #[derive(GraphQLQuery)]
 #[graphql(
