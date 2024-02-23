0f40319b59f3a60d0cd91155a1d9899c632e6c98
Andrei <92177534+andrei-21@users.noreply.github.com>
2024-02-23 18:10:06 +0000

    Add pigeon (#94)

---
+++ pigeon/src/lib.rs
@@ -0,0 +1,20 @@
+use graphql::perro::OptionToError;
+use graphql::schema::{assign_lightning_address, AssignLightningAddress};
+use graphql::{build_async_client, post};
+use honey_badger::asynchronous::Auth;
+
+pub async fn assign_lightning_address(backend_url: &str, auth: &Auth) -> graphql::Result<String> {
+    let token = auth.query_token().await?;
+    let client = build_async_client(Some(&token))?;
+    let data = post::<AssignLightningAddress>(
+        &client,
+        backend_url,
+        assign_lightning_address::Variables {},
+    )
+    .await?;
+    let address = data
+        .assign_lightning_address
+        .ok_or_permanent_failure("Unexpected backend response: empty")?
+        .address;
+    Ok(address)
+}
